2dc2fbdd450748566b0ab0653df6623648e6dccf
Akshatha nayak <akshanayaki@gmail.com>
2018-03-02 04:45:23 +0530

    sponsor section added

--- a_data/script.js
+++ a_data/script.js
@@ -15,6 +15,26 @@ jQuery(function($) {
 		navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'] //navigation text
 	});
 
+    $(document).ready(function () {
+    $('#myCarousel').carousel({
+        interval: 10000
+    })
+    $('.fdi-Carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
+});
+
 	//2. Urgent Causes - Isotope Plugin
 
     jQuery(window).load(function() {
